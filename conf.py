7fa34dc5b8058c69d04267d2e4174c41a9ab0bef
Martin Landa <landa.martin@gmail.com>
2016-09-23 18:36:04 +0200

    zmena zapati

--- conf.py
+++ conf.py
@@ -10,7 +10,7 @@ from utils import get_month_year, get_year
 
 # General information about the project.
 project = u'Školení QGIS pro pokročilé'
-copyright = u'%d, Alžbeta Gardoňová, Ľudmila Furtkevičová, Oto Kaláb a Martin Landa (GISMentors.eu)' % get_year()
+copyright = u'2015-%d GISMentors' % get_year()
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
